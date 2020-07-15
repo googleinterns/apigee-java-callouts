7c1ceb31b90114261644ccb671a1eea27fc02fa2
Morgan <morganlaw@users.noreply.github.com>
2020-07-15 14:55:32 -0600

    Add Baseclass Hello sample proxy (#4)
    Add Baseclass Hello sample proxy
    
    Co-authored-by: Morgan Law <morganklaw@google.com>

---
+++ baseclass-samples/java-callouts-baseclass-sample-hello/invoke.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo Using org and environment configured in /setup/setenv.sh
+
+source ../../setup/setenv.sh
+
+curl http://$org-$env.$api_domain/java-callouts-baseclass-sample-hello?name="Alice"
